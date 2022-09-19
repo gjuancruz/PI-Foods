4d31d636893a823cd4df9abfebed5c592ed269d3
gjuancruz <tatugalaz@gmail.com>
2022-09-19 12:37:53 -0300

    diets page fixes

--- client/src/Components/DietDetail/DietDetail.jsx
+++ client/src/Components/DietDetail/DietDetail.jsx
@@ -4,25 +4,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import DietCards from '../DietCard/DietCard';
 import styles from './DietDetail.module.css'
 
-const DietDetail = ()=>{
+const DietDetail = () => {
     const dispatch = useDispatch()
-React.useEffect(()=>{
-    dispatch(getAllDietsAct())
-    return
-}, [])
+    React.useEffect(() => {
+        dispatch(getAllDietsAct())
+        return
+    }, [])
 
-const diets = useSelector(state=> state.diets)
+    const diets = useSelector(state => state.diets)
 
-return(
-    <div className={styles.box}>
-        <h1>In APPetite, you can find:</h1>
-        {diets && diets.map(r =>
-            <div key ={r.id}>
-            <DietCards diets={r.diets} />
+    return (
+        <div className={styles.container}>
+            <div className={styles.box}>
+                <h1>In APPetite, you can find:</h1>
+                {diets && diets.map(r =>
+                    <div key={r.id}>
+                        <DietCards diets={r.diets} />
+                    </div>
+                )}
+                <h1> and more...</h1>
             </div>
-            )}
-        <h1> and more...</h1>
-    </div>
+        </div>
     )
 }
 
